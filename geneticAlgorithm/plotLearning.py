0fa1728f70b648710c25fa96906f7893059a35dc
August Ravn Mader <aumad17@student.sdu.dk>
2021-05-24 10:54:32 +0200

    plot updated

--- geneticAlgorithm/plotLearning.py
+++ geneticAlgorithm/plotLearning.py
@@ -2,19 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 
-#data = pd.read_csv("data.csv")
-
-data = pd.read_csv("dataExtraExtraState.csv")
-#data = pd.read_csv("dataFinale.csv")
-#data = pd.read_csv("data(MutationSTD0.25).csv")
-
-#data = pd.read_csv("dataFirstIteration(MutationSTD2.5).csv")
-#print(data.info())
-#print(data.head())
-##print(len(data.columns) - 1)
-#print(data.shape[0])
-#print(data['2'])
-
+data = pd.read_csv("data.csv")
 
 mean = []
 stdPos = []
@@ -38,11 +26,8 @@ for i in range(len(data.columns) - 1):
     stdX.append(i)
     size = []
     for j in y:
-        #print(j)
         counter = y.count(j)
         size.append(counter)
-        #print(counter)
-    #plt.plot(x, y, 'o', color='C0', markersize=2)
     size = [n * 1 for n in size]
     plt.scatter(x,y,s=size,c='C0')
 
@@ -50,7 +35,6 @@ print(np.mean(best))
 print(np.mean(meanScore))
 print(np.mean(standardDeviation))
 
-plt.title("Fitness plot for the GA with the extra state")
 plt.title("Fitness plot for the GA")
 plt.ylabel("Fitness score")
 plt.xlabel("Generations")
@@ -59,10 +43,6 @@ plt.show()
 plt.plot(meanX, mean, 'o-',markersize=2)
 plt.fill_between(stdX, stdNeg, stdPos, alpha=.1)
 
-#plt.plot(stdX, stdPos, 'o-', color='black',markersize=2)
-#plt.plot(stdX, stdNeg, 'o-', color='black',markersize=2)
-
-plt.title("Mean and Standard deviation plot for GA")
 plt.title("Mean and Standard deviation plot for GA")
 plt.ylabel("Fitness score")
 plt.xlabel("Generations")
